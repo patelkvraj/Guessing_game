caa4493e2ec885093562825431f177a3221d99a4
Vraj Patel <mycsstuff@outlook.com>
2022-12-15 20:45:33 -0500

    Add project files.

---
+++ Guessing_game/Source.cpp
@@ -0,0 +1,34 @@
+/*
+AUTHOR:
+_______
+
+	Vraj K Patel
+
+TOTAL FILES THE PROGRAM CONTAINS:
+_________________________________
+
+	Total files:	1
+		1) Source.cpp
+
+
+ABOUT THE PROGRAM:
+__________________
+
+	Part - 2
+		Created on	:	12/15/2022
+		Title (Name):	Guessing_game
+		Description	:	The program randomly select an integer from 1 to 15. The user 
+						will keep guessing numbers until they find the program's number, 
+						and the program will tell them each time if their guess was too 
+						high or too low:
+							1 2 3 4 5 6 -> 7 <- 8 9 10 11 12 13 14 15
+		Purpose		:	Little game to show the working of algorithm. 
+		Challenges	:	-
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	return 0;	//	no return 
+}
